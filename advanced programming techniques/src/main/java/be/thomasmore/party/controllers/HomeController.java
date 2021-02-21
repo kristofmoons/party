55c7fe2456ddad1bbfda22b5054d1e109f6eacf2
kristofmoons <75735495+kristofmoons@users.noreply.github.com>
2021-02-21 20:34:36 +0100

    Add files via upload

--- advanced programming techniques/src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ advanced programming techniques/src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -1,17 +1,35 @@
 package be.thomasmore.party.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
+    private final int mySpecialNumber = 729;
 
     @GetMapping("/")
-    public String home (){
+    public String home(Model model) {
+        model.addAttribute("mySpecialNumber", mySpecialNumber);
         return "home";
     }
-    @GetMapping("/")
-    public String about (){
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("mySpecialNumber", mySpecialNumber);
         return "about";
+    }
+
+    @GetMapping("/pay")
+    public String pay(Model model) {
+        model.addAttribute("mySpecialNumber", mySpecialNumber);
+        return "pay";
+    }
+    @GetMapping("/venuedetails/{venueName}")
+    public String venueDetails(Model model,
+                               @PathVariable String venueName) {
+        model.addAttribute("venueName", venueName);
+        return "venuedetails";
     }
 }
