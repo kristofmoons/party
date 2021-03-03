3ea581aebf122243ffca502043f601e696277841
kristofmoons <r0696321@student.thomasmore.be>
2021-03-03 15:44:48 +0100

    clean up

--- party2021Q3-bundel_2_and_deploy/src/main/java/be/thomasmore/party/repositories/ArtistRepository.java
+++
@@ -1,7 +0,0 @@
-package be.thomasmore.party.repositories;
-
-import be.thomasmore.party.model.Artist;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ArtistRepository extends CrudRepository<Artist, Integer> {
-}
