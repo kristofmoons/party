05ef382976c89e250a55352990eaa7d2bd1dff83
kristofmoons <r0696321@student.thomasmore.be>
2021-03-09 15:46:24 +0100

    artist filter

--- src/main/java/be/thomasmore/party/controllers/ArtistController.java
+++ src/main/java/be/thomasmore/party/controllers/ArtistController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 
@@ -23,20 +24,37 @@ public class ArtistController {
                                 @PathVariable(required = false) Integer id) {
         if (id == null) return "artistdetails";
 
-        Optional<Artist> optionalArtist = artistRepository.findById(id);
+        Optional <Artist> optionalArtist = artistRepository.findById(id);
         if (optionalArtist.isPresent()) {
             long nrOfArtists = artistRepository.count();
             model.addAttribute("artist", optionalArtist.get());
             model.addAttribute("prevId", id > 1 ? id - 1 : nrOfArtists);
             model.addAttribute("nextId", id < nrOfArtists ? id + 1 : 1);
+
         }
         return "artistdetails";
     }
 
     @GetMapping("/artistlist")
     public String artistList(Model model) {
-        Iterable<Artist> artists = artistRepository.findAll();
+        Iterable <Artist> artists = artistRepository.findAll();
+        long nrOfArtists = artistRepository.count();
         model.addAttribute("artists", artists);
+        model.addAttribute("showFilters", false);
+        model.addAttribute("nrOfArtists", nrOfArtists);
+        return "artistlist";
+    }
+    @GetMapping("/artistlist/filter")
+    public String artistListFilter(Model model, @RequestParam (required = false) String keyword){
+        Iterable <Artist> jos = artistRepository.findByKeyword(keyword);
+
+        int nrOfArtists =0;
+        for (Artist a :jos){
+            nrOfArtists++;
+        }
+        model.addAttribute("nrOfArtists", nrOfArtists);
+        model.addAttribute("artists", jos);
+        model.addAttribute("showFilters", true);
         return "artistlist";
     }
 }
