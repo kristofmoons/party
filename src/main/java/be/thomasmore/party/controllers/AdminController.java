5f28bbb97e172c7bac7ee7a393a479941c71d79e
kristofmoons <r0696321@student.thomasmore.be>
2021-03-27 18:49:55 +0100

    je kan party editten

--- src/main/java/be/thomasmore/party/controllers/AdminController.java
+++ src/main/java/be/thomasmore/party/controllers/AdminController.java
@@ -7,9 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -23,12 +21,26 @@ public class AdminController {
 
     @GetMapping("/partyedit/{id}")
     public String partyEdit(Model model,
-                            @PathVariable Integer id) {
-        if (id == null) return "admin/partyedit";
-
+                            @PathVariable int id) {
+        logger.info("partyEdit " + id);
         Optional<Party> optionalParty = partyRepository.findById(id);
         if (optionalParty.isPresent()) {
             model.addAttribute("party", optionalParty.get());
+        }
+        return "admin/partyedit";
+    }
+
+    @PostMapping("/partyedit/{id}")
+    public String partyEditPost(Model model,
+                                @PathVariable int id,
+                                @RequestParam String partyName) {
+        logger.info("partyEditPost " + id + " -- new name=" + partyName);
+        Optional<Party> optionalParty = partyRepository.findById(id);
+        if (optionalParty.isPresent()) {
+            Party party = optionalParty.get();
+            party.setName(partyName);
+            partyRepository.save(party);
+            model.addAttribute("party", party);
         }
         return "admin/partyedit";
     }
