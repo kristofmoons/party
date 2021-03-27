9545d3bd164de60ad7e86163efd7d7d0c7f0b73f
kristofmoons <r0696321@student.thomasmore.be>
2021-03-27 18:43:52 +0100

    party template geupdated met list en een edit toegevoegd voor de admin

--- src/main/java/be/thomasmore/party/controllers/AdminController.java
+++ src/main/java/be/thomasmore/party/controllers/AdminController.java
@@ -1,24 +1,36 @@
 package be.thomasmore.party.controllers;
 
+import be.thomasmore.party.model.Party;
+import be.thomasmore.party.repositories.PartyRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
+    private Logger logger = LoggerFactory.getLogger(AdminController.class);
 
-    private Logger logger = LoggerFactory.getLogger(VenueController.class);
+    @Autowired
+    private PartyRepository partyRepository;
 
-    @RequestMapping({"/admin"})
-    public String admin(Model model) {
-        return "admin";
-    }
-    @GetMapping({"partyedit/{id}"})
-    public String partyedit(Model model) {
-        return "partyedit";
+    @GetMapping("/partyedit/{id}")
+    public String partyEdit(Model model,
+                            @PathVariable Integer id) {
+        if (id == null) return "admin/partyedit";
+
+        Optional<Party> optionalParty = partyRepository.findById(id);
+        if (optionalParty.isPresent()) {
+            model.addAttribute("party", optionalParty.get());
+        }
+        return "admin/partyedit";
     }
 
 }
